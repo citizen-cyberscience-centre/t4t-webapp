b654d945fa070766d929508efd4e376db208262d
Daniel Lombraña González <teleyinex@gmail.com>
2011-12-27 12:26:47 +0100

    Only show the galleria when there are images available. Closes #3
    The script was showing the galleria instance when resources.json was
    ready. However, the images take some time to get generated, and galleria
    was trying to load them when the images do not exist yet. This commit
    fixes the problem.

--- js/test4theory.js
+++ js/test4theory.js
@@ -31,25 +31,28 @@ function getBoincData() {
 
 function getResources() {
         $.getJSON('job/resources.json', function(data){
-                        $("#waiting").hide();
-                        $("#container").show();
-                        // Output the jobMetaData
-                        $("#info").append('<strong>Beam:</strong> ' + data.jobMetaData.Beam + '</br>');
-                        $("#info").append('<strong>Process:</strong> ' + data.jobMetaData.Process + '</br>');
-                        $("#info").append('<strong>Energy:</strong> ' + data.jobMetaData.Energy + '</br>');
-                        $("#info").append('<strong>Cuts:</strong> ' + data.jobMetaData.Cuts + '</br>');
-                        $("#info").append('<strong>Generator:</strong> ' + data.jobMetaData.Generator + '</br>');
-                        $("#info").append('<strong>Version:</strong> ' + data.jobMetaData.Version + '</br>');
-                        $("#info").append('<strong>Tune:</strong> ' + data.jobMetaData.Tune + '</br>');
-                        $("#info").append('<strong>Events:</strong> ' + data.jobMetaData.Events + '</br>');
+                        // Check if there are some images to show, otherwise, keep the waiting message
+                        if (data.sprites.histograms.length != 0) {
+                                $("#waiting").hide();
+                                $("#container").show();
+                                // Output the jobMetaData
+                                $("#info").append('<strong>Beam:</strong> ' + data.jobMetaData.Beam + '</br>');
+                                $("#info").append('<strong>Process:</strong> ' + data.jobMetaData.Process + '</br>');
+                                $("#info").append('<strong>Energy:</strong> ' + data.jobMetaData.Energy + '</br>');
+                                $("#info").append('<strong>Cuts:</strong> ' + data.jobMetaData.Cuts + '</br>');
+                                $("#info").append('<strong>Generator:</strong> ' + data.jobMetaData.Generator + '</br>');
+                                $("#info").append('<strong>Version:</strong> ' + data.jobMetaData.Version + '</br>');
+                                $("#info").append('<strong>Tune:</strong> ' + data.jobMetaData.Tune + '</br>');
+                                $("#info").append('<strong>Events:</strong> ' + data.jobMetaData.Events + '</br>');
 
-                        // Retrieve histograms and append them to the figures
-                        // array
-                        $.each(data.sprites.histograms, function(index, histogram){
-                                figures.push({'image': "job" + histogram});
-                                });
-                        // Enable all the divs
-                        return figures;
+                                // Retrieve histograms and append them to the figures
+                                // array
+                                $.each(data.sprites.histograms, function(index, histogram){
+                                        figures.push({'image': "job" + histogram});
+                                        });
+                                // Enable all the divs
+                                return figures;
+                        }
                         });
 }
 
